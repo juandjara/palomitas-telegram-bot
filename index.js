4a3edd6738b96cfcfc5134ebe96aa6da2ebae91c
juandjara <juanorigami@gmail.com>
2016-10-26 19:22:09 +0200

    add tests

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ app.get('/', function(req, res){
               sample:'/url-ejemplo-servicio'});
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('app listening at http://%s:%s', host, port);
 });
+
+module.exports = server;
---
+++ tests/root-test.js
@@ -0,0 +1,24 @@
+var chai = require('chai');
+var expect = chai.expect;
+var axios = require('axios');
+
+describe('microservice', function(){
+  var server;
+  var port = process.env.PORT || 3000;
+
+  before(() => { server = require('../index')});
+  after(() => { server.close(); });
+
+  it('should return http code 200 OK on /', function() {
+    return axios.get(`http://localhost:${port}/`).then((res) => {
+      expect(res.status).to.equal(200);
+    });
+  });
+
+  it('stauts should be ok on /', function() {
+    return axios.get(`http://localhost:${port}/`).then((res) => {
+      expect(res.data.status).to.equal("ok");
+    });
+  });
+
+});
